adca27e9d4f9285241c6215823b9537d6a64f845
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2020-05-04 00:11:39 -0400

    use jemallocator

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,8 @@
+extern crate jemallocator;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 mod levenshtein_distance;
 
 use levenshtein_distance::LevenshteinDistance;
